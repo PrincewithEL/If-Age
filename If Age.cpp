7775fc9873d7d2b73d9f112f0efe62fbd6ad03c0
PrincewithEL <86285043+PrincewithEL@users.noreply.github.com>
2021-10-26 20:29:14 +0300

    Add files via upload

---
+++ If Age.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Asher Yisrael Kutswa ~ 137187
+
+int main(){
+	int age;
+	cout<<"Please Enter Age : ";
+	cin>>age;
+	cout<<"\n";
+	if(age>=4){
+		cout<<"Admitted, proceed to Registration.";
+	}else{
+		cout<<"Declined, Age is not Permitted.\n";
+	}
+	
+	return 0;
+}
+
+
